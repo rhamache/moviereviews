2fbf84008fb24a4d9060080110080a1d62b95d63
rhamache@ualberta.ca <rhamache@ualberta.ca>
2016-11-16 18:42:11 -0700

    initial commit

---
+++ MovieReviews.Web/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace MovieReviews.Web.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ MovieReviews.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieReviews.Web.MvcApplication" Language="C#" %>
